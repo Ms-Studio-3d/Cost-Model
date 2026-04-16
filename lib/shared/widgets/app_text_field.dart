c4fac17f1defd56b22baf0de971f19df343f19b3
Ms-Studio-3d <mohsalah_12@yahoo.com>
2026-04-16 14:57:21 +0200

    Create app_text_field.dart

---
+++ lib/shared/widgets/app_text_field.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AppTextField extends StatelessWidget {
+  const AppTextField({
+    super.key,
+    required this.controller,
+    required this.label,
+    this.hint,
+    this.keyboardType,
+  });
+
+  final TextEditingController controller;
+  final String label;
+  final String? hint;
+  final TextInputType? keyboardType;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      keyboardType: keyboardType,
+      decoration: InputDecoration(
+        labelText: label,
+        hintText: hint,
+      ),
+    );
+  }
+}
