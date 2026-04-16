80f40ab6a6b52aa78565c87186f66637eab66981
Ms-Studio-3d <mohsalah_12@yahoo.com>
2026-04-16 14:12:19 +0200

    Update project_type_card.dart

--- lib/shared/widgets/project_type_card.dart
+++ lib/shared/widgets/project_type_card.dart
@@ -30,7 +30,7 @@ class ProjectTypeCard extends StatelessWidget {
                 width: 52,
                 height: 52,
                 decoration: BoxDecoration(
-                  color: theme.colorScheme.primary.withOpacity(0.12),
+                  color: theme.colorScheme.primary.withValues(alpha: 0.12),
                   borderRadius: BorderRadius.circular(16),
                 ),
                 child: Icon(
