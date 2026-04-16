1b81eddc5e2500ef8bd64c1aabe659360d0aa05c
Ms-Studio-3d <mohsalah_12@yahoo.com>
2026-04-16 13:58:30 +0200

    Create app_strings.dart

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,10 @@
+class AppStrings {
+  const AppStrings._();
+
+  static const appName = 'Cost Model';
+  static const appSubtitle = 'Architecture pricing and cost model';
+  static const startNewProject = 'Start New Project';
+  static const buildings = 'Buildings';
+  static const masterPlan = 'Master Plan';
+  static const combined = 'Combined';
+}
