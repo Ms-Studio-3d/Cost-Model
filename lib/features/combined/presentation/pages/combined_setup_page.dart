422b63e33b29cb263842cad626d0d40b69e54172
Ms-Studio-3d <mohsalah_12@yahoo.com>
2026-04-19 10:28:50 +0200

    Update combined_setup_page.dart

--- lib/features/combined/presentation/pages/combined_setup_page.dart
+++ lib/features/combined/presentation/pages/combined_setup_page.dart
@@ -26,6 +26,7 @@ class _CombinedSetupPageState extends State<CombinedSetupPage> {
   late final TextEditingController _masterPlanProfitMarginController;
   late final TextEditingController _masterPlanOtherExpensesController;
 
+  
   CombinedProject _project = CombinedProject(
     buildingsProject: const b_models.BuildingsProject(),
     masterPlanProject: const mp_models.MasterPlanProject(),
